d6bd4eeebb2d731ab8f1e9f4c5171cc0e1ae1541
Cristian Cardellino <crscardellino@gmail.com>
2015-12-03 20:40:44 -0300

    Changed the train_test_split to return a pair of datasets

--- crossvalidation.lua
+++ crossvalidation.lua
@@ -15,22 +15,25 @@ function mltools.crossvalidation.train_test_split(dataset, train_ratio)
     local shuffle = torch.randperm(dataset:size())
     local train_size = math.floor(shuffle:size(1) * train_ratio)
     local test_size = shuffle:size(1) - train_size
-    local features_size = dataset[1][1]:size()[1]
 
-    local X_train = torch.Tensor(train_size, features_size)
-    local X_test = torch.Tensor(test_size, features_size)
-    local y_train = torch.Tensor(train_size)
-    local y_test = torch.Tensor(test_size)
+    local train_dataset = {}
+    local test_dataset = {}
+
+    function train_dataset:size()
+        return train_size
+    end
+
+    function test_dataset:size()
+        return test_size
+    end
 
     for i = 1, train_size do
-        X_train[i] = dataset[shuffle[i]][1]:clone()
-        y_train[i] = dataset[shuffle[i]][2]
+        train_dataset[i] = {dataset[shuffle[i]][1]:clone(), dataset[shuffle[i]][2]}
     end
 
     for i = train_size + 1, train_size + test_size do
-        X_test[i-train_size] = dataset[shuffle[i]][1]:clone()
-        y_test[i-train_size] = dataset[shuffle[i]][2]
+        test_dataset[i-train_size] = {dataset[shuffle[i]][1]:clone(), dataset[shuffle[i]][2]}
     end
 
-    return X_train, X_test, y_train, y_test
+    return train_dataset, test_dataset
 end
